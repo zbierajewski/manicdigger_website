52969078d4f6039937a1094101a9a3834ae2e1ca
croxxx <i.am.croxxx@aol.de>
2015-08-30 14:25:37 +0200

    Fixed broken login

--- classes/Login.php
+++ classes/Login.php
@@ -67,9 +67,9 @@ class Login
 
                 // database query, getting all the info of the selected user (allows login via email address in the
                 // username field)
-                $sql = "SELECT user_name, user_email, user_password_hash
+                $sql = "SELECT user_name, user_password_hash
                         FROM users
-                        WHERE user_name = '" . $user_name . "' OR user_email = '" . $user_name . "';";
+                        WHERE user_name = '" . $user_name . "';";
                 $result_of_login_check = $this->db_connection->query($sql);
 
                 // if this user exists
@@ -84,7 +84,6 @@ class Login
 
                         // write user data into PHP SESSION (a file on your server)
                         $_SESSION['user_name'] = $result_row->user_name;
-                        $_SESSION['user_email'] = $result_row->user_email;
                         $_SESSION['user_login_status'] = 1;
 
                     } else {
