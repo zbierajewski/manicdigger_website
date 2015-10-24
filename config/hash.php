29341a1eb13e83cbe95319b35f254a8ed8299e3c
croxxx <i.am.croxxx@aol.de>
2015-10-24 17:26:56 +0200

    Introduce a GAME_VERSION variable

--- config/hash.php
+++ config/hash.php
@@ -3,6 +3,8 @@
 /**
  * Configuration for: Server list
  *
- * LIST_KEY: key used for creating server hashes
+ * LIST_KEY:        key used for creating server hashes
+ * GAME_VERSION:    client version which shall be treated as release version
  */
 define("LIST_KEY", "insertyourtopsecretkeyhere"); //Private key that is unique to the webserver, never share this!
+define("GAME_VERSION", "2015-10-14"); //Current release version of the game. Used for sorting on serverlist
